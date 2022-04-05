fa6ba048cbe815c1c5b04342ad76ddf4fdfd432e
linson7017 <linson7017@126.com>
2022-04-05 13:20:09 +0800

    add time interval

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import random
-import sys
 import CountWidget
 import PyQt5.QtCore as QtCore
 import pyttsx3
+import random
+import sys
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication, QDialog, QLabel
 from number_to_word import num2word
@@ -33,13 +33,14 @@ class MainDialog(QDialog):
                 self.Labels.append(label)
                 self.ui.NumberLayout.addWidget(label, i, j)
         self.ui.RandomPickBtn.clicked.connect(self.RandomPick)
-        self.ui.ClearBtn.clicked.connect(self.Clear)
+        self.ui.ResetBtn.clicked.connect(self.Reset)
 
-        timer = QTimer()
-        timer.timeout.connect(self.Scan)  # 计时器挂接到槽：update
-        self.ui.ScanBtn.clicked.connect(lambda: timer.start(500))
-        self.ui.RandomPickBtn.clicked.connect(lambda: timer.stop())
-        self.ui.ClearBtn.clicked.connect(lambda: timer.stop())
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.Refresh)  # 计时器挂接到槽：update
+        self.ui.ScanBtn.clicked.connect(self.Scan)
+        self.ui.RandomPickBtn.clicked.connect(self.StopScan)
+        self.ui.ResetBtn.clicked.connect(self.StopScan)
+        self.ui.ScanIntervalSlider.valueChanged.connect(self.InternalChanged)
 
     def _get_color_style(self, name):
         return "QLabel{{ font-size:25px;font-weight:bold;font-family:Roman times;background-color:{} }}".format(name)
@@ -48,9 +49,9 @@ class MainDialog(QDialog):
         label.setStyleSheet(self._get_color_style(color))
 
     def _say_number(self, number):
-        if self.ui.LanguageCombox.currentIndex() == 0:
+        if self.ui.LanguageComboBox.currentIndex() == 0:
             engine(number)
-        elif self.ui.LanguageCombox.currentIndex() == 1:
+        elif self.ui.LanguageComboBox.currentIndex() == 1:
             engine(num2word(number))
         else:
             engine(number)
@@ -63,18 +64,34 @@ class MainDialog(QDialog):
             self.repaint()
         self._say_number(self.CurrentLabel + 1)
 
-    def Scan(self):
+    def Refresh(self):
         if self.CurrentLabel >= -1 and self.CurrentLabel < 100:
             self.ChangeLabel(self.CurrentLabel + 1)
         else:
             self.CurrentLabel = 0
 
+    def Scan(self, checked):
+        if checked:
+            self.timer.start(self.ui.ScanIntervalSlider.value())
+            self.ui.ScanIntervalSlider.setEnabled(False)
+        else:
+            self.StopScan()
+
+    def StopScan(self):
+        self.timer.stop()
+        self.ui.ScanIntervalSlider.setEnabled(True)
+        self.ui.ScanBtn.setChecked(False)
+
     def RandomPick(self):
         num = random.randint(0, 100)
         self.ChangeLabel(num)
 
-    def Clear(self):
-        print("Clear")
+    def InternalChanged(self):
+        self.ui.InternalText.setText('{} ms'.format(self.ui.ScanIntervalSlider.value()))
+
+    def Reset(self):
+        self._set_label_color(self.Labels[self.CurrentLabel], self.DefaultColor)
+        self.CurrentLabel = -1
 
 
 if __name__ == '__main__':
